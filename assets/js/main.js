a1919161fefcaeec4c1d37a9ec15bbd3936b6204
goranradmanovic <goranradmanovic@gmail.com>
2019-01-22 21:12:13 +0100

    Changing mistypo in js function

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,14 +11,14 @@ function togglePaints() {
 		if (event.target.checked) {
 			pageElements[1].setAttribute('checked', true); //Checkbox
 			//Header link
-			pageElements[0].setAttribute('checked', 'https://www.piet-mondrian.org/composition-ii-in-red-blue-and-yellow.jsp'); //Link href
+			pageElements[0].setAttribute('href', 'https://www.piet-mondrian.org/composition-ii-in-red-blue-and-yellow.jsp'); //Link href
 			pageElements[0].text = 'Composition II in Red, Blue, and Yellow, 1929'; //Link text
 			pageElements[2].classList.add('close'); //First (default) paint
 			pageElements[3].classList.remove('close'); //Second paint
 		} else {
 			pageElements[1].setAttribute('checked', false); //Checkbox
 			//Header link
-			pageElements[0].setAttribute('checked', 'https://www.piet-mondrian.org/composition-with-large-red-plane-yellow-black-gray-and-blue.jsp');
+			pageElements[0].setAttribute('href', 'https://www.piet-mondrian.org/composition-with-large-red-plane-yellow-black-gray-and-blue.jsp');
 			pageElements[0].text = 'Composition with Large Red Plane, Yellow, Black, Gray, and Blue, 1921'; //Link text
 			pageElements[3].classList.add('close'); //First (default) paint
 			pageElements[2].classList.remove('close'); //Second paint
